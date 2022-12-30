59157c398f08bbc9d8711f2dc1c96a0ff99110f3
Isabella Persaud <isabellapersaud041600@gmail.com>
2022-12-30 16:05:31 -0500

    commit tree ff42d04dc39b3d5cd4ba12009cebfa7c8ec475bc

---
+++ index.js
@@ -0,0 +1,46 @@
+function add(){
+    return "something";
+}
+function subtract(){
+    return "something";
+}
+function multiply(){
+    return "something";
+}
+function divide() {
+    return "something";
+}
+function add(a, b){
+    return a + b;
+
+}
+function subtract(a,b) {
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+function increment(n){
+    n++
+    return n;
+}
+function decrement(n){
+    n--
+    return n;
+}
+function makeInt(n){
+    return n;
+}
+function preserveDecimal(n){
+    parseFloat(n);
+    return n;
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
